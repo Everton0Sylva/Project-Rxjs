c6b9647c26cd917c8951b10f1cecf0bd60b841d0
Everton Silva <cerberuzwolf.dev@gmail.com>
2024-11-01 11:37:53 -0300

    Treino Teste
    Incluído teste no service

--- src/app/services/apirequest.service.spec.ts
+++ src/app/services/apirequest.service.spec.ts
@@ -1,6 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApirequestService } from './apirequest.service';
+import { debounceTime, last, of, switchMap } from 'rxjs';
+import { Character } from '../model/character';
+import { Methods } from '../model/Methods';
 
 describe('ApirequestService', () => {
   let service: ApirequestService;
@@ -12,5 +15,22 @@ describe('ApirequestService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+
+
+  it('Teste da função Fetch', () => {
+    let methods = Methods;
+    let lista = new Array<any>();
+    service.Fetch("/?name=Alien", methods.Get).pipe(
+      last(),
+      switchMap((data: any) => {
+        return of([...data.results])
+      }))
+      .subscribe(result => {
+        lista = [...result];
+      })
+    expect(lista).toBeGreaterThanOrEqual(0);
+
   });
 });
--- src/app/views/views.component.spec.ts
+++ src/app/views/views.component.spec.ts
@@ -1,23 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewsComponent } from './views.component';
+import { ApirequestService } from '../services/apirequest.service';
+import { inject } from '@angular/core';
+import { NgxUiLoaderModule, NgxUiLoaderService } from 'ngx-ui-loader';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { ViewsRoutingModule } from './views-routing.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('ViewsComponent', () => {
   let component: ViewsComponent;
   let fixture: ComponentFixture<ViewsComponent>;
 
+  let apirequestService: ApirequestService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ViewsComponent]
+      declarations: [ViewsComponent],
+      providers: [ApirequestService],
+      imports: [
+        CommonModule,
+        FormsModule,
+        ViewsRoutingModule,
+        NgxUiLoaderModule,
+        TypeaheadModule.forRoot()
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ViewsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    apirequestService = fixture.debugElement.injector.get(ApirequestService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('Check Search', () => {
+    // ngxUiLoaderService = inject(NgxUiLoaderService);
+    // apirequestService = new ApirequestService();
+    /*  spyOn(apirequestService, 'Fetch').and.returnValues((list: any) => {
+        return list.length > 0;
+      });*/
   });
 });
